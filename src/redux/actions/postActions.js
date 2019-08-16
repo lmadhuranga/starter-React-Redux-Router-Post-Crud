e5f6be9c78a8e8536432a08a57dac34b760f5e73
Madhuranga Sendheera <lilan.maduranga@gmail.com>
2019-08-16 17:10:32 +0530

    All redux features moved to redux forlder and updated missing paths

---
+++ src/redux/actions/postActions.js
@@ -0,0 +1,52 @@
+import { FETCH_POSTS, NEW_POST, VIEW_POST, UPDATE_POST }  from './types';
+import { appConfig } from '../../config/globel.conf';
+const entity = 'posts';
+const entityUrl = `${appConfig.app.host.url}/${entity}`;
+
+export const fetchPosts = () => dispatch => {
+    fetch(`${entityUrl}`)
+    .then(res => res.json())
+    .then(posts => dispatch({
+        type:FETCH_POSTS,
+        payload: posts
+    }));
+}
+
+export const create = (formData) => dispatch => {
+    fetch(`${entityUrl}`, {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json'
+        },
+        body: JSON.stringify(formData)
+    })
+    .then(res => res.json())
+    .then(post => dispatch({
+        type:NEW_POST,
+        payload: post
+    }));
+}
+
+export const update = (id, formData) => dispatch => {
+    return fetch(`${entityUrl}/${id}`, {
+        method: 'PUT',
+        headers: {
+            'content-type': 'application/json'
+        },
+        body: JSON.stringify(formData)
+    })
+    .then(res => res.json())
+    .then(post => dispatch({
+        type:UPDATE_POST,
+        payload: post
+    }));
+}
+
+export const fetchPost = (postId) => dispatch => { 
+    fetch(`${entityUrl}/${postId}`)
+    .then(res => res.json())
+    .then(post => dispatch({
+        type: VIEW_POST,
+        payload: post
+    }));
+}
---
+++ src/redux/reducers/postReducer.js
@@ -0,0 +1,35 @@
+import { FETCH_POSTS, NEW_POST, VIEW_POST, UPDATE_POST }  from '../actions/types';
+
+const initialSate = {
+    items: [],
+    item: {}
+}
+
+export default function(state = initialSate, action) {
+    // console.log('action',action.type ,action.payload);
+    // console.log('initialSate',initialSate);
+    switch(action.type) {
+        case FETCH_POSTS: 
+            return {
+                ...state,
+                items: action.payload
+            }
+        case NEW_POST: 
+            return {
+                ...state,
+                item: action.payload
+            }
+        case UPDATE_POST: 
+            return {
+                ...state,
+                item: action.payload
+            }
+        case VIEW_POST: 
+            return {
+                ...state,
+                item: action.payload
+            }
+        default:
+            return state;
+    }
+}
