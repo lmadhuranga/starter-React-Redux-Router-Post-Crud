7d0ebccfd7a6f2924329d61d1c19b45b80487a4d
Madhuranga Sendheera <lilan.maduranga@gmail.com>
2019-08-16 21:22:22 +0530

    fix6-update-brand-name-navbar * updated with app name using configuration file

--- src/Pages/Navgation.js
+++ src/Pages/Navgation.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import { NavLink, Link } from 'react-router-dom';
- 
+import {  appConfig  } from '../config/globel.conf'
+
 class Navgation extends Component {
   
   render() { 
     return (
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <Link className="navbar-brand" to="#">Navbar</Link>
+        <Link className="navbar-brand" to="/">{appConfig.app.name}</Link>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
